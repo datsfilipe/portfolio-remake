8716e03d54cb2d2a98a3bbfc483076c4c125744d
datsfilipe <datsfilipe.foss@proton.me>
2024-07-24 10:12:00 -0300

    refactor: update projects page to jsx

---
+++ src/pages/ui/projects.tsx
@@ -0,0 +1,14 @@
+import { SectionHeading } from '@features/section/ui/sectionHeading'
+
+export default function Projects() {
+	return (
+		<>
+			<h1 className='text-3xl font-bold mb-8 mt-8 px-4 py-2 bg-blue-600 w-fit'>Projects!</h1>
+			{['Web', 'Tools', 'Themes'].map(category => (
+				<div key={category}>
+					<SectionHeading title={category} />
+				</div>
+			))}
+		</>
+	)
+}
