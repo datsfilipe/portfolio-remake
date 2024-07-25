62423d00463c1b40c17f016a3d9741a74fd6265c
datsfilipe <datsfilipe.foss@proton.me>
2024-07-25 00:48:19 -0300

    refactor: not showing projects now (not implemented)

--- src/pages/ui/projects.tsx
+++ src/pages/ui/projects.tsx
@@ -1,6 +1,9 @@
 import { SectionHeading } from '@features/section/ui/sectionHeading'
 
 export default function Projects() {
+	const isDev = import.meta.env.MODE === 'development'
+	if (!isDev) return null
+
 	return (
 		<>
 			<h1 className='text-3xl font-bold mb-8 mt-8 px-4 py-2 bg-blue-600 w-fit'>Projects!</h1>
