61b4429e1e6cf86b89f683264be6cf8dac337de2
datsfilipe <datsfilipe.foss@proton.me>
2024-07-25 18:52:52 -0300

    refactor: update styles for articles, remove margins from headings

--- src/pages/ui/posts/[slug].tsx
+++ src/pages/ui/posts/[slug].tsx
@@ -26,7 +26,7 @@ export default function BlogPost() {
 			</div>
 			<section className='max-w-full'>
 				<article
-					className='prose xl:prose-lg prose-quoteless prose-neutral dark:prose-invert prose-p:text-justify mb-8'
+					className='prose lg:prose-xl prose-quoteless prose-neutral dark:prose-invert prose-p:text-justify mb-8 prose-headings:my-0'
 					// biome-ignore lint/security/noDangerouslySetInnerHtml: intentional
 					dangerouslySetInnerHTML={{ __html: post.content }}
 				/>
