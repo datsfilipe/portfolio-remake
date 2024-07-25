d65de3ab2425e69991019f6422cff30d8d2a1a16
datsfilipe <datsfilipe.foss@proton.me>
2024-07-24 23:41:48 -0300

    feat: complete working shareable notes support

--- src/pages/ui/shareable-notes/[slug].tsx
+++ src/pages/ui/shareable-notes/[slug].tsx
@@ -1,8 +1,19 @@
 import { useLoaderData } from 'react-router-dom'
+import type { Note } from '@shared/lib/schemas'
 
 export const layout = 'shareableNote'
 
 export default function ShareableNote() {
-	const note = useLoaderData()
-	return <h1>BrainNote</h1>
+	const note = useLoaderData() as Note
+
+	return (
+		<>
+			<h1 className='text-4xl font-bold bg-blue-600 px-4 py-2 mb-6'>{note.title}</h1>
+			<article
+				className='prose xl:prose-lg prose-quoteless prose-neutral dark:prose-invert prose-p:text-justify mb-8'
+				// biome-ignore lint/security/noDangerouslySetInnerHtml: intentional
+				dangerouslySetInnerHTML={{ __html: note.content }}
+			/>
+		</>
+	)
 }
--- src/widgets/ui/layouts/shareableNote.tsx
+++ src/widgets/ui/layouts/shareableNote.tsx
@@ -9,12 +9,7 @@ export default function ShareableNoteLayout(props: { title: string; children: Re
 			<Header title={SITE_TITLE} />
 			<main className='flex lg:flex-row flex-col mb-12 max-w-3xl lg:max-w-fit mx-auto px-4  min-h-[75dvh] sm:my-8 my-4'>
 				<Sidebar />
-				<section className='flex flex-col space-y-2'>
-					<h1 className='text-4xl font-bold bg-blue-600 px-4 py-2 mb-6'>{props.title}</h1>
-					<article className='prose xl:prose-lg prose-quoteless prose-neutral dark:prose-invert prose-p:text-justify mb-8'>
-						{props.children}
-					</article>
-				</section>
+				<section className='flex flex-col space-y-2'>{props.children}</section>
 			</main>
 			<Footer />
 		</>
