a4811ae36a8c475ddb185f221e90ba33c57c66b6
datsfilipe <datsfilipe.foss@proton.me>
2024-07-26 01:04:14 -0300

    refactor: increase footer height

--- src/features/footer/ui/footer.tsx
+++ src/features/footer/ui/footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
 	const today = new Date()
 
 	return (
-		<footer className='flex w-full p-4 mt-4 text-stone-400'>
+		<footer className='flex w-full p-4 mt-4 pb-14 text-stone-400'>
 			<span className='mx-auto'>&copy; {today.getFullYear()} Filipe Lima. All rights reserved.</span>
 		</footer>
 	)
