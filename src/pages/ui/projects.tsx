a92516c85ea4753c598c303f337d102bafa194f2
datsfilipe <datsfilipe.foss@proton.me>
2024-07-25 23:51:55 -0300

    refactor: enhance projects message

--- src/pages/ui/projects.tsx
+++ src/pages/ui/projects.tsx
@@ -1,17 +1,26 @@
+import { IoIosWarning } from 'react-icons/io'
 import { SectionHeading } from '@features/section/ui/sectionHeading'
 
 export default function Projects() {
 	const isDev = import.meta.env.MODE === 'development'
-	if (!isDev) return null
 
 	return (
 		<>
 			<h1 className='text-3xl font-bold mb-8 mt-8 px-4 py-2 bg-blue-600 w-fit'>Projects!</h1>
-			{['Web', 'Tools', 'Themes'].map(category => (
-				<div key={category}>
-					<SectionHeading title={category} />
-				</div>
-			))}
+			{!isDev ? (
+				<p className='text-lg flex space-x-2 items-center'>
+					<span>This page is under construction.</span>
+					<IoIosWarning size={24} className='text-yellow-500' />
+				</p>
+			) : (
+				<>
+					{['Web', 'Tools', 'Themes'].map(category => (
+						<div key={category}>
+							<SectionHeading title={category} />
+						</div>
+					))}
+				</>
+			)}
 		</>
 	)
 }
